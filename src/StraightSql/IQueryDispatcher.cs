e1aa90cd758992d23df19ae105ce476ece432e5f
Brendan J. Baker <brendan@brendanbaker.com>
2016-03-04 18:59:50 -0500

    List query support and basic tests.

--- src/StraightSql/IQueryDispatcher.cs
+++ src/StraightSql/IQueryDispatcher.cs
@@ -1,11 +1,14 @@
 ﻿namespace StraightSql
 {
 	using System;
+	using System.Collections.Generic;
+	using System.Data.Common;
 	using System.Threading.Tasks;
 
 	public interface IQueryDispatcher
 	{
 		Task<Int64> CountAsync(IQuery query);
 		Task ExecuteAsync(IQuery query);
+		Task<IList<T>> ListAsync<T>(IQuery query, Func<DbDataReader, T> reader);
 	}
 }
