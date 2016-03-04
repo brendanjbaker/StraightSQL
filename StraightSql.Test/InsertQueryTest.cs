e263811bb314f28fce97a2d98ee72da57dfef0f6
Brendan J. Baker <brendan@brendanbaker.com>
2016-03-03 22:54:47 -0500

    Add testing project; includes insertion test.

---
+++ StraightSql.Test/InsertQueryTest.cs
@@ -0,0 +1,27 @@
+﻿namespace StraightSql.Test
+{
+	using System;
+	using System.Threading.Tasks;
+	using Xunit;
+
+	public class InsertQueryTest
+	{
+		[Fact]
+		public async Task InsertQueryTestAsync()
+		{
+			var queryDispatcher =
+				new QueryDispatcher(
+					new ConnectionFactory(ConnectionString.Default));
+
+			var queries = new String[]
+			{
+				"DROP TABLE IF EXISTS insert_query_test;",
+				"CREATE TABLE insert_query_test (value TEXT NOT NULL);",
+				"INSERT INTO insert_query_test VALUES ('StraightSql');"
+			};
+
+			foreach (var query in queries)
+				await queryDispatcher.ExecuteAsync(new Query(query));
+		}
+	}
+}
