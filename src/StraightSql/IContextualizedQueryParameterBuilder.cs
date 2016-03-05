fc42b6f749fd3e6065d384ceb97f8a0253be90f6
Brendan J. Baker <brendan@brendanbaker.com>
2016-03-05 13:10:15 -0500

    Contextualized query builder, with test.

---
+++ src/StraightSql/IContextualizedQueryParameterBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace StraightSql
+{
+	using System;
+
+	public interface IContextualizedQueryParameterBuilder
+	{
+		IContextualizedQuery Build();
+		IContextualizedQueryParameterBuilder SetParameter<T>(String name, T value);
+	}
+}
