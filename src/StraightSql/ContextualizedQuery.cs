2f2d5d3912555099650bf845bd06e6e242305256
Brendan J. Baker <brendan@brendanbaker.com>
2016-03-05 12:49:39 -0500

    Contextualized query -- pairs a query with its dispatcher.

---
+++ src/StraightSql/ContextualizedQuery.cs
@@ -0,0 +1,71 @@
+﻿namespace StraightSql
+{
+	using Npgsql;
+	using System;
+	using System.Collections.Generic;
+	using System.Data.Common;
+	using System.Threading.Tasks;
+
+	public class ContextualizedQuery
+		: IContextualizedQuery
+	{
+		private readonly IQuery query;
+		private readonly IQueryDispatcher queryDispatcher;
+
+		public ContextualizedQuery(IQuery query, IQueryDispatcher queryDispatcher)
+		{
+			this.query = query;
+			this.queryDispatcher = queryDispatcher;
+		}
+
+		public IEnumerable<NpgsqlParameter> Parameters
+		{
+			get { return query.Parameters; }
+		}
+
+		public String Text
+		{
+			get { return query.Text; }
+		}
+
+		public async Task<Boolean> AnyAsync()
+		{
+			return await queryDispatcher.AnyAsync(query);
+		}
+
+		public async Task<Int64> CountAsync()
+		{
+			return await queryDispatcher.CountAsync(query);
+		}
+
+		public async Task ExecuteAsync()
+		{
+			await queryDispatcher.ExecuteAsync(query);
+		}
+
+		public async Task<T> FirstAsync<T>(Func<DbDataReader, T> reader)
+		{
+			return await queryDispatcher.FirstAsync(query, reader);
+		}
+
+		public async Task<T> FirstOrDefaultAsync<T>(Func<DbDataReader, T> reader)
+		{
+			return await queryDispatcher.FirstOrDefaultAsync(query, reader);
+		}
+
+		public async Task<IList<T>> ListAsync<T>(Func<DbDataReader, T> reader)
+		{
+			return await queryDispatcher.ListAsync(query, reader);
+		}
+
+		public async Task<T> SingleAsync<T>(Func<DbDataReader, T> reader)
+		{
+			return await queryDispatcher.SingleAsync(query, reader);
+		}
+
+		public async Task<T> SingleOrDefaultAsync<T>(Func<DbDataReader, T> reader)
+		{
+			return await queryDispatcher.SingleOrDefaultAsync(query, reader);
+		}
+	}
+}
---
+++ test/StraightSql.Test/ContextualizedQueryTest.cs
@@ -0,0 +1,54 @@
+﻿namespace StraightSql.Test
+{
+	using System;
+	using System.Threading.Tasks;
+	using Xunit;
+
+	public class ContextualizedQueryTest
+	{
+		[Fact]
+		public async Task ContextualizedQueryTestAsync()
+		{
+			var queryDispatcher =
+				new QueryDispatcher(
+					new ConnectionFactory(ConnectionString.Default));
+
+			var setupQueries = new String[]
+			{
+				"DROP TABLE IF EXISTS contextualized_query_test;",
+				"CREATE TABLE contextualized_query_test (id INT NOT NULL, value TEXT NOT NULL);",
+				"INSERT INTO contextualized_query_test VALUES (1, 'Sigma');",
+				"INSERT INTO contextualized_query_test VALUES (2, 'Nu');",
+				"INSERT INTO contextualized_query_test VALUES (3, 'Iota');",
+				"INSERT INTO contextualized_query_test VALUES (4, 'Delta');"
+			};
+
+			foreach (var setupQuery in setupQueries)
+				await queryDispatcher.ExecuteAsync(new Query(setupQuery));
+
+			var query =
+				new QueryBuilder()
+					.SetQuery(@"
+						SELECT id, value
+						FROM contextualized_query_test
+						WHERE value = :greekLetter;")
+					.SetParameter("greekLetter", "Delta")
+					.Build();
+
+			var contextualizedQuery = new ContextualizedQuery(query, queryDispatcher);
+
+			var item = await contextualizedQuery.FirstAsync(reader =>
+			{
+				return new
+				{
+					id = (Int32)reader["id"],
+					value = (String)reader["value"]
+				};
+			});
+
+			Assert.NotNull(item);
+			Assert.Equal(item.id, 4);
+			Assert.Equal(item.value, "Delta");
+		}
+	}
+}
