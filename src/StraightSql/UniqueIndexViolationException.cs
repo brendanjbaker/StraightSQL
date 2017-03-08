e22a744810da74bb16683417fd1a023b823a9fc2
Brendan J. Baker <brendan@brendanbaker.com>
2017-03-07 23:15:20 -0500

    Catch the general PostgresException for violating a unique index, and re-throw a specific exception to represent the corresponding circumstance.

---
+++ src/StraightSql/UniqueIndexViolationException.cs
@@ -0,0 +1,13 @@
+﻿namespace StraightSql
+{
+	using Npgsql;
+	using System;
+
+	public class UniqueIndexViolationException
+		: Exception
+	{
+		public UniqueIndexViolationException(PostgresException innerException)
+			: base("A unique index constraint was violated.", innerException)
+		{ }
+	}
+}
