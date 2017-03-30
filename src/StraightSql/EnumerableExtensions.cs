74ce554d6201fcb407ef550dea6a6139bdb76082
Brendan J. Baker <brendan@brendanbaker.com>
2017-03-29 23:33:07 -0400

    Extension methods for IEnumerable<T>: None() and Multiple(). Implementation does not use Count(), so slow or infinite collections don't prevent successful and performant usage.

---
+++ src/StraightSql/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace StraightSql
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public static class EnumerableExtensions
+	{
+		public static Boolean Multiple<T>(this IEnumerable<T> items)
+		{
+			return items.Skip(1).Any();
+		}
+
+		public static Boolean None<T>(this IEnumerable<T> items)
+		{
+			return !items.Any();
+		}
+	}
+}
