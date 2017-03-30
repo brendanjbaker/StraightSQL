5b4dfd36edac65bab2af720870441a1e2ddcbf66
Brendan J. Baker <brendan@brendanbaker.com>
2017-03-30 17:34:22 -0400

    Provide a type converter source that supports using TypeInfo.IsAssignableFrom() to cast between types.

---
+++ src/StraightSql/Conversion/Source/AssignableTypeConverterSource.cs
@@ -0,0 +1,21 @@
+﻿namespace StraightSql.Conversion.Source
+{
+	using System;
+	using System.ComponentModel;
+	using System.Reflection;
+
+	public class AssignableTypeConverterSource
+		: ITypeConverterSource
+	{
+		public TypeConverter TryGet<T>(Type type)
+		{
+			if (!typeof(T).IsAssignableFrom(type))
+				return null;
+
+			return new FunctionalTypeConverter(type, typeof(T), instance =>
+			{
+				return (T)instance;
+			});
+		}
+	}
+}
